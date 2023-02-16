4e38fff059fc99f0c54044f45ad7373837306dfc
metacode22 <alohajune22@gmail.com>
2023-02-16 23:00:01 +0900

    chore: msw 초기화

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,4 @@
-import { worker } from 'mocks/browser';
 import type { AppProps } from 'next/app';
-
-if (process.env.NODE_ENV === 'development') {
-  (async () => {
-    await worker.start();
-  })();
-}
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
